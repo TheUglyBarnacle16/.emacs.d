a97b7a77f6959acde842b2983be78c68d701ce14
Jacob McCormick <jacob@jacobmccormick.xyz>
2020-07-23 20:48:01 -0500

    Remove ignored files

--- elpa/which-key-20200702.219/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20200702.219" "Display available keybindings in popup" '((emacs "24.4")) :commit "ce25fb260003eb965e1c104804f690d9405ec8b4" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
