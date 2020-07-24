a94c84ddedb188f29c036269f946d8a78351d96c
Jacob McCormick <jacob@jacobmccormick.xyz>
2020-07-23 20:40:47 -0500

    reduced garbage collection to reduce initial load times

--- init.el
+++ init.el
@@ -1,3 +1,20 @@
+;;; Help emacs start faster ;;;
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+        gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
 ;; Necessary
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -11,7 +28,8 @@
   (package-install 'use-package))
 ;;;;;
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;;;;;
 (custom-set-variables
